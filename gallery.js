9894942bebed55b4258384e1fb7390546d177abf
vhegde <vhegde@uw.edu>
2022-12-13 21:31:07 -0800

    Completed with comments

--- gallery.js
+++ gallery.js
@@ -1,3 +1,10 @@
+/**
+ * gallery.js file used by gameplay.html webpage of "Outbreak:PTO" website
+ * Creator: Vivek Hegde
+ *
+ * This file contains methods to both automatically and manually transition to the next screenshot
+ * picture in the gallery.
+ */
 "use strict";
 (function() {
 
@@ -19,6 +26,10 @@
     setInterval(switchRight, 3000);
   }
 
+  /**
+   * Function to switch the current screenshot image to the previous image in the gallery.
+   * If the current image is the first in the gallery the image will be switched to the last image in the gallery.
+   */
   function switchLeft() {
     let newPosition = currentPosition - 1;
     if (newPosition == -1) {
@@ -29,6 +40,10 @@
     currentPosition = newPosition;
   }
 
+  /**
+   * Function to switch the current screenshot image to the next image in the gallery.
+   * If the current image is the last in the gallery the image will be switched to the first image in the gallery.
+   */
   function switchRight() {
     let newPosition = currentPosition + 1;
     if (newPosition == 5) {
